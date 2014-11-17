8b75fb7b9924938a8edef5ca86c7ca82f183d3f9
Daniel Zamorano <daniel.zamorano@wisertogether.com>
2014-11-17 13:20:08 -0600

    Firebase clients

--- app/assets/js/clients.js
+++ app/assets/js/clients.js
@@ -3,5 +3,11 @@
 /**/
 
 (function() {
+    var dataClientsPath = 'https://the-ark-dev.firebaseio.com/legion/clients',
+    dataClients = new Firebase(dataClientsPath);
+
+    dataClients.on("value", function(snapshot) {
+      console.log(snapshot.val());
+    });
 
 })();
