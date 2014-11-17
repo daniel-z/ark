6fdc86f33ddcd12e95c17a9edb02346b28674d07
Daniel Zamorano <daniel.zamorano@wisertogether.com>
2014-11-17 15:21:45 -0600

    Adding firebase data loading, handlebars templating & underscore.

--- app/assets/js/clients.js
+++ app/assets/js/clients.js
@@ -4,10 +4,17 @@
 
 (function() {
     var dataClientsPath = 'https://the-ark-dev.firebaseio.com/legion/clients',
-    dataClients = new Firebase(dataClientsPath);
+    dataClients = new Firebase(dataClientsPath),
+    template = Handlebars.compile($('#customer-row').html());
+
+    var printCustomers = function(customers){
+        $('#client-table tbody .no-data').remove();
+
+        $('#client-table tbody').append(template({customers: customers}));
+    };
 
     dataClients.on("value", function(snapshot) {
       console.log(snapshot.val());
+      printCustomers(snapshot.val());
     });
-
 })();
