43ec05ad179308a869c658c1b0c7311512e81ab9
Daniel Zamorano <daniel.zamorano@wisertogether.com>
2014-11-17 23:31:01 -0600

    adding table stuff

--- app/assets/js/clients.js
+++ app/assets/js/clients.js
@@ -10,9 +10,33 @@
     var printCustomers = function(customers){
         $('#client-table tbody .no-data').remove();
         $('#client-table tbody').append(template({customers: customers}));
+
+        var table = $('#client-table').DataTable({
+             "paging": false,
+             "ordering": false,
+             "info": false
+        });
+
+        table.on( 'draw', function () {
+            var body = $( table.table().body() );
+
+            body.unhighlight();
+            body.highlight( table.search() );
+        } );
+
     };
+
+    // $('#client-table').filterTable({
+    //     inputSelector: '#filter',
+    //     callback: function(term, table) {
+    //         console.log('term',term);
+    //     }
+    // });
 
     dataClients.once("value", function(snapshot) {
       printCustomers(snapshot.val());
     });
 })();
+
+$(document).ready( function () {
+} );
